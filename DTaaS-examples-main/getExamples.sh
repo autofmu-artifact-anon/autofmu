990040df65e68ed3fed9bcfc725de635457b662d
anonymous <anonymous@example.com>
2026-05-28 23:23:14 +0800

    update

---
+++ DTaaS-examples-main/getExamples.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /root/Desktop/
+wget -P workspace https://github.com/INTO-CPS-Association/DTaaS-examples/archive/refs/heads/main.zip
+unzip workspace/main.zip -d workspace
+mv workspace/DTaaS-examples-main workspace/examples
+rm workspace/main.zip
